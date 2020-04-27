034844c2c4d3b329b91acf0e5a1d9f92af447d47
Huskey231 <whuskey6@gmail.com>
2020-04-26 19:22:35 -0600

    Left right button example from the course.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,52 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  // Defines the function that we imported at the top
-  const [ counter, setCounter ] = useState(0)
-  // Counter is set to zero because of the useState(0) part, and setCounter is set to a function that modifies state
-// After the first time when this function is called useState grabs the current value of counter and assigns it here
-
-  // Increments the counter through grabbing the current counter point
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  // () => .... is the function to incrememnt being executed by setTimeout and 1000 is the timeout length (1 second)
-  // When setcounter is called it changes the state which the forces a re-render to take place
-  console.log('rendering...', counter)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
-// The number being rendered will always be the old number hence return {counter} while the new number is being processed,
-// once the method completes it is then printed on screen
-
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
 )
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
