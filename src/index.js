6234d83422b33435a65203bc08a5617bb2a8a091
Huskey231 <whuskey6@gmail.com>
2020-05-03 12:47:14 -0600

    Documentation

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 // Allows button to run a function without having to assign it a function through having it call a function that returns a function
-const ex1Button = () => {
-  const handler = () => console.log("I am a test button rendering test through a function!")
+// This way we can reuse button functionality that is mostly similar
+const ex1Button = (word) => {
+  const handler = () => console.log("I am a ", word, " button rendering test through a function!")
   return handler
 }
 
@@ -11,7 +12,8 @@ const App = (props) => {
   return (
     <div>
       <div>
-        <button onClick={ex1Button()}>Test Button</button>
+        <button onClick={ex1Button('test')}>Test Button</button>
+        <button onClick={ex1Button('sample')}>Sample Button</button>
       </div>
     </div>
   )
