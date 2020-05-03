bbf981970e288f4a9bf8cc9c3de3686676530cf6
Huskey231 <whuskey6@gmail.com>
2020-05-03 12:41:56 -0600

    Playing with button assignments

--- src/index.js
+++ src/index.js
@@ -1,51 +1,17 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
+// Allows button to run a function without having to assign it a function through having it call a function that returns a function
+const ex1Button = () => {
+  const handler = () => console.log("I am a test button rendering test through a function!")
+  return handler
 }
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
-
 const App = (props) => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
   return (
     <div>
       <div>
-        {left}
-        <Button onClick={handleLeftClick} text='left' />
-        <Button onClick={handleRightClick} text='right' />
-        {right}
-        <History allClicks={allClicks} />
+        <button onClick={ex1Button()}>Test Button</button>
       </div>
     </div>
   )
