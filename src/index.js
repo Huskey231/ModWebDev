e11538097fc54eafd1073f9d7a1e1e0dc162c212
Huskey231 <whuskey6@gmail.com>
2020-05-18 23:36:05 -0600

    Completed 1.7

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const newButton = (props) => (
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-)
-
-const statistics = (props) => <div>{props.text}, : , {props.value}</div>
-
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // Done the verbose way, returns a function so that it can work with the button call
+  const NeutralHandler = () => {
+    const handler = () => {
+      setNeutral(neutral + 1)
+    }
+    return handler
+  }
+
+  // Shortened option, since we only have one return we can right the function like this rather than
+  // having to write the handler out fully and return it
+  const GoodHandler = () =>
+    () => setGood(good + 1)
+
+  const Average = () => {
+    return (good - bad)/(good + bad + neutral)
+  }
+
+  const PerPositive = () => {
+    return good / (good + bad + neutral)
+  }
+
   return (
     <div>
       <h2>Please tell us how we did!</h2>
-      <newButton handleClick={ () => setBad(bad + 1)} text='Bad'/> 
-      <newButton handleClick={ () => setNeutral(neutral + 1)} text='Neutral'/>
-      <newButton handleClick={ () => setGood(good + 1)} text='Good'/>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <button onClick={NeutralHandler()}>Neutral</button>
+      <button onClick={GoodHandler()}>Good</button>
       <br></br>
       <h2>Current Statistics</h2>
-      <statistics text='Good' value={good} />
-      <statistics text='Neutral' value ={neutral}/>
-      <statistics text='Bad' value ={bad}/>
+      Bad: {bad}<br/>
+      Neutral: {neutral}<br/>
+      Good: {good}<br/>
+      Average: {Average()}<br/>
+      Positive: {PerPositive()}
     </div>
   )
 }
